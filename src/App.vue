0b67488c5a54772cf443bb68d5cc64bf3fe4e939
rezasetyawan <setyawanreza960@gmail.com>
2023-07-24 10:24:16 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+ <main class="font-open-sans">
+  <TheHeader />
+ </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+<template>
+    <header class="grid grid-cols-3 justify-items-center p-5">
+        <h1 class="text-lg justify-self-start">Keep</h1>
+        <input class="bg-accent-color1 focus:outline-none p-2 w-full" placeholder="Search your notes">
+    </header>
+</template>
