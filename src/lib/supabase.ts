069d027cb934be1b5d690c00f1f4499ca743800c
rezasetyawan <setyawanreza960@gmail.com>
2023-08-02 21:18:02 +0700

    change typo

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
     import.meta.env.VITE_APP_SUPABASE_URL as string,
-    import.meta.env.VITE_APP_SUPABSE_API_KEY as string
+    import.meta.env.VITE_APP_SUPABASE_API_KEY as string
 )
